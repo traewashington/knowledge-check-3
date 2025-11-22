941cf7bc6c49a4cae9d8358decd819d5f5431a40
Taylor Washington <taylorraewashington@gmail.com>
2025-11-21 19:18:26 -0800

    answered questions for knowledge check 3

--- KC3: Form Validation/script.js
+++ KC3: Form Validation/script.js
@@ -1,13 +1,38 @@
 /*
 1. Print a message inside the div with the error class whenever the user submits a blank form (JS)
+
+/*create a function for the users input to pass through*/
+/* declare error class within the function */
+/*create a condition inside the funtion if the user submits a blank form. (Possible if statement)
+/* use alert () stating "error: no information inputed into form" to the user */
+
+
 2. Handle failure icon visibility (CSS). Hint: check the HTML for the error-classes
+
+/* On styles page, declare styling to the class. 
+failure-icon {
+
+/* add styling to the .fas */
+
+failure-icon { 
+color: black;
+}
+
 3. Handle the success icon visibility if a user completes all inputs and submits (CSS)
+
+
 4. Ensure password must have more that 8 letters
+
+/*use alert() to let the user know their password should be more than 8 characters. Input message inside of ()*/
+
 5. Email can not be empty
+/* Use DOM manipulation to retrieve an email */
+ document.getElementById(email)
+ /* create conditional statement for when the email is empty*/
+ /* create an alert() stating to send an error message */
 
 Bonus
 Change the success/failure icons according to successful input values or incorrect input values
 */
 
 
-test 
